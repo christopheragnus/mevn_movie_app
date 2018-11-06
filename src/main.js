c945e0ee3cc422e814f2fc928e6a39bb3a11a4a9
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-11-06 17:38:56 +1100

    intro boilerplate and setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
+
 import Vue from 'vue'
+import Vuetify from 'vuetify';
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue);
+Vue.use(Vuetify);
 
 new Vue({
   router,
