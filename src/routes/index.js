5ef617cdb85ce00e21b3b6e17543b22900621a74
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-11-07 22:26:53 +1100

    add link to description

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home.vue';
 import Contact from '@/components/Contact';
 import AddMovie from '@/components/AddMovie';
+import Movie from '@/components/Movie';
 
 
 Vue.use(Router)
@@ -25,6 +26,11 @@ export default new Router({
       path: '/movies/add',
       name: 'AddMovie',
       component: AddMovie
-    }
-  ]
+    },
+    {
+      path: '/movies/:id',
+      name: 'Movie',
+      component: Movie
+    },
+  ],
 })
