912741bbf6bb57158f02a1aa3faa35a802648d80
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-11-07 16:19:32 +1100

    add axios and endpoints

--- src/components/AddMovie.vue
+++ src/components/AddMovie.vue
@@ -15,11 +15,15 @@
        <v-select
          label="Movie Release Year"
          v-model="release_year"
+         required 
+         :rules="releaseRules"
          :items="years"
        ></v-select>
        <v-text-field
          label="Movie Genre"
          v-model="genre"
+         required
+         :rules="genreRules"
        ></v-text-field>
        <v-btn
          @click="submit"
@@ -32,6 +36,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data: () => ({
         valid: true,
@@ -41,6 +47,13 @@ export default {
         release_year: '',
             nameRules: [
         v => !!v || 'Movie name is required',
+        ],
+        genreRules: [
+            v => !!v || 'Movie genre is required',
+            v => (v && v.length <= 80) || 'Genre must be less than equal to 80 characters.',
+        ],
+        releaseRules: [
+            v => !!v ||  'Movie release year is required',
         ],
         select: null,
         years: [
@@ -54,7 +67,27 @@ export default {
         submit() {
             if (this.$refs.form.validate()) {
             // Perform next action
+            return axios({
+                method: 'post',
+                data: {
+                    name: this.name,
+                    description: this.description,
+                    release_year: this.release_year,
+                    genre: this.genre,
+                },
+                url: 'http://localhost:8081/movies',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then(() => {
+                this.$router.push({ name: 'Home' });
+                this.$refs.form.reset();
+            })
+            .catch(() => {        
+            });
         }
+        return true;
     },
         clear() {
             this.$refs.form.reset();
