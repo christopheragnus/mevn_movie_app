4a579f977481032a155c062e551ec7d0411668ac
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-11-08 17:38:30 +1100

    add submit and clear methods to register form

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Home from '@/components/Home.vue';
 import Contact from '@/components/Contact';
 import AddMovie from '@/components/AddMovie';
 import Movie from '@/components/Movie';
+import Register from '@/components/Register';
 
 
 Vue.use(Router)
@@ -31,6 +32,11 @@ export default new Router({
       path: '/movies/:id',
       name: 'Movie',
       component: Movie
+    },
+    {
+      path: '/users/register',
+      name: 'Register',
+      component: Register,
     },
   ],
 })
