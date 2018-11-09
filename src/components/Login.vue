b69bba4a870fd436cd2ef85aafb9100dd0b96976
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-11-09 16:29:11 +1100

    create a register view page

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+     <v-form v-model="valid" ref="form" lazy-validation>
+       <v-text-field
+         label="Email"
+         v-model="email"
+         :rules="emailRules"
+         required
+       ></v-text-field>
+       <v-text-field
+         label="Password"
+         v-model="password"
+         required
+       ></v-text-field>
+       <v-btn
+         @click="submit"
+         :disabled="!valid"
+       >
+         submit
+       </v-btn>
+       <v-btn @click="clear">clear</v-btn>
+     </v-form>
+</template>
